23fa6affeab22de0e809f65bed33ec91c4019685
samnang2003 <samnang200319@gmail.com>
2026-03-15 16:08:17 +0700

    v5 update fix error chat panel1

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "orderflow-ui-v6";
+const CACHE_NAME = "orderflow-ui-v7";
 const STATIC_ASSETS = [
   "/",
   "/manifest.json",
@@ -46,6 +46,7 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
 
 
